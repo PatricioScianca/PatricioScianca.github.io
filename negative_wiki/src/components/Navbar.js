94df0794b60849ff1e8e36f9764add45d7253b84
PatricioScianca <patricio.sciancalepore@gmail.com>
2022-06-23 18:20:41 -0300

    Firsts big changes

---
+++ negative_wiki/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+function Navbar() {
+    return(
+        <div>
+            <header className="App-header">
+                <div className='container'>
+                    <h1>-5e<span>tools</span></h1>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Navbar;
