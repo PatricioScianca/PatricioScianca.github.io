ce48bad76b258ba74e350a3d4b00e4de6808f166
PatricioScianca <patricio.sciancalepore@gmail.com>
2022-06-29 11:43:40 -0300

    finished icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import { FaUsers, FaStar, FaMedal, FaClipboardList, FaBomb, FaAtom, FaBook, FaRegUserCircle } from "react-icons/fa";
+import { FaUsers, FaStar, FaMedal, FaClipboardList, FaBomb, FaAtom, FaBook, FaRegUserCircle, FaDungeon, FaDragon, FaMap, FaSkullCrossbones, FaSlidersH, FaBookmark, FaMagic, FaCoins } from "react-icons/fa";
 
 function Home() {
     return(
@@ -23,7 +23,7 @@ function Home() {
                     </a>
                     <a className='Item-Button btn'>
                         <div className='Icons'><FaClipboardList/></div>
-                        <h4 id='dos-lineas'>Options <br/>& Features</h4>
+                        <h4>Options / Features</h4>
                     </a>
                     <a className='Item-Button btn'>
                         <div className='Icons'><FaRegUserCircle/></div>
@@ -34,7 +34,7 @@ function Home() {
                         <h4>Items</h4>
                     </a>
                     <a className='Item-Button btn'>
-                        <div className='Icons'><FaBook/></div>
+                        <div className='Icons'><FaMagic/></div>
                         <h4>Spells</h4>
                     </a>
                     <a className='Item-Button btn'>
@@ -44,45 +44,45 @@ function Home() {
                 </div>
             </div>
 
-            {/* <div className='lower-row'>
+            <div className='lower-row'>
                 <div className='top-text'>
                     <div className='reference-text'>Players</div>
                 </div>
                 <div className='left-row'>
                     <a className='Item-Button btn'>
-                        <div className='Icons'><FaUsers/></div>
-                        <h4>Races</h4>
-                    </a>
-                    <a className='Item-Button btn'>
-                        <div className='Icons'><FaStar/></div>
-                        <h4>Classes</h4>
-                    </a>
-                    <a className='Item-Button btn'>
-                        <div className='Icons'><FaMedal/></div>
-                        <h4>Feats</h4>
-                    </a>
-                    <a className='Item-Button btn'>
-                        <div className='Icons'><FaClipboardList/></div>
-                        <h4 id='dos-lineas'>Options <br/>& Features</h4>
-                    </a>
-                    <a className='Item-Button btn'>
-                        <div className='Icons'><FaRegUserCircle/></div>
-                        <h4>Backgrounds</h4>
-                    </a>
-                    <a className='Item-Button btn'>
-                        <div className='Icons'><FaBomb/></div>
-                        <h4>Items</h4>
+                        <div className='Icons'><FaDungeon/></div>
+                        <h4>Adventures</h4>
                     </a>
                     <a className='Item-Button btn'>
                         <div className='Icons'><FaBook/></div>
-                        <h4>Spells</h4>
+                        <h4>Books</h4>
                     </a>
                     <a className='Item-Button btn'>
-                        <div className='Icons'><FaAtom/></div>
-                        <h4>Psionics</h4>
+                        <div className='Icons'><FaBookmark/></div>
+                        <h4>Quick Reference</h4>
+                    </a>
+                    <a className='Item-Button btn'>
+                        <div className='Icons'><FaSkullCrossbones/></div>
+                        <h4>Conditions</h4>
+                    </a>
+                    <a className='Item-Button btn'>
+                        <div className='Icons'><FaDragon/></div>
+                        <h4>Bestiary</h4>
+                    </a>
+                    <a className='Item-Button btn'>
+                        <div className='Icons'><FaMap/></div>
+                        <h4>DM Screen</h4>
+                    </a>
+                    <a className='Item-Button btn'>
+                        <div className='Icons'><FaCoins/></div>
+                        <h4>Loot Generator</h4>
+                    </a>
+                    <a className='Item-Button btn'>
+                        <div className='Icons'><FaSlidersH/></div>
+                        <h4>CR Calculator</h4>
                     </a>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 };
